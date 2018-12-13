991ef50d3df198026140362cda78ca93a3232a8c
Eric Nguyen <33969138+airicbear@users.noreply.github.com>
2018-12-12 23:05:03 -0500

    Decrease focus delay

--- js/twice.js
+++ js/twice.js
@@ -128,7 +128,7 @@ let setFocusDelay = (id, delay = 1000) => {
   }, delay);
 };
 
-let getFocusDelay = (index) => 800 + (index * 130);
+let getFocusDelay = (index) => 800 + (index * 110);
 
 /* ~~~~~~~~ Spaghetti code ~~~~~~~~ */
 
