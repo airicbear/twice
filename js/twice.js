cc09bfe803a23892cd338162aae1a68a137958b8
Eric Nguyen <33969138+airicbear@users.noreply.github.com>
2018-12-12 19:13:22 -0500

    The Year of Yes

--- js/twice.js
+++ js/twice.js
@@ -1,3 +1,75 @@
+const TwiceMembers = [
+  "Momo",
+  "Dahyun",
+  "Nayeon",
+  "Mina",
+  "Chaeyoung",
+  "Sana",
+  "Jihyo",
+  "Jeongyeon",
+  "Tzuyu"
+]
+
+const TwiceMemberInfo = {
+  "Nayeon": {
+    "birthName": "Im Nayeon (임나연)",
+    "dateOfBirth": "September 22, 1995",
+    "nationality": "South Korean",
+    "birthplace": "Seoul, South Korea",
+  },
+  "Jeongyeon": {
+    "birthName": "Yoo Kyung Wan (유경완)",
+    "legalName": "Yoo Jung Yeon (유정연)",
+    "dateOfBirth": "November 1, 1996",
+    "nationality": "South Korean",
+    "birthplace": "Suwon, South Korea",
+  },
+  "Momo": {
+    "birthName": "Hirai Momo (히라이 모모)",
+    "dateOfBirth": "November 9, 1996",
+    "nationality": "Japanese",
+    "birthplace": "Kyōtanabe, Kyoto, Japan",
+  },
+  "Sana": {
+    "birthName": "Minatozaki Sana (미나토자키 사나)",
+    "dateOfBirth": "December 29, 1996",
+    "nationality": "Japanese",
+    "birthplace": "Tennōji-ku, Osaka, Japan",
+  },
+  "Jihyo": {
+    "birthName": "Park Ji Soo (박지수)",
+    "legalName": "Park Ji Hyo (박지효)",
+    "dateOfBirth": "February 1, 1997",
+    "nationality": "South Korean",
+    "birthplace": "Guri, Gyeonggi-do, South Korea",    
+  },
+  "Mina": {
+    "birthName": "Myoui Mina (묘이 미나)",
+    "englishName": "Sharon Myoui",
+    "dateOfBirth": "March 24, 1997",
+    "nationality": "Japanese",
+    "birthplace": "San Antonio, Texas, United States",
+  },
+  "Dahyun": {
+    "birthName": "Kim Dahyun (김다현)",
+    "dateOfBirth": "May 28, 1998",
+    "nationality": "South Korean",
+    "birthplace": "Seongnam, Gyeonggi-do, South Korea",
+  },
+  "Chaeyoung": {
+    "birthName": "Son Chae Young (손채영)",
+    "dateOfBirth": "April 23, 1999",
+    "nationality": "South Korean",
+    "birthplace": "Seoul, South Korea",
+  },
+  "Tzuyu": {
+    "birthName": "Chou Tzuyu (저우쯔위)",
+    "dateOfBirth": "June 14, 1999",
+    "nationality": "Taiwanese",
+    "birthplace": "East District, Tainan, Taiwan",
+  },
+}
+
 // Some videos are unavailable when embedded...
 const twiceVideos = [
   'mAKsZ26SabQ', // Yes or Yes
@@ -43,4 +115,177 @@ function randomVideo(target_id, video_id_list = twiceVideos) {
   vid.allowFullscreen = true;
   target.appendChild(vid);
   console.log(vid.src);
+}
+
+/**
+ * Focus image with delay to allow smooth anchor jump
+ * @param {string} id
+ * @param {number} delay
+ **/
+let setFocusDelay = (id, delay = 1000) => {
+  setTimeout(() => {
+    document.getElementById(id).focus();
+  }, delay);
+};
+
+/* ~~~~~~~~ Spaghetti code ~~~~~~~~ */
+
+function Section(id = "Twice", tabIndex = -1) {
+  let section = document.createElement("section");
+  section.id = id;
+  if (tabIndex === 0) {
+    section.tabIndex = tabIndex;
+  }
+  return section;
+}
+
+function Link(href = "", focus = "", focusDelay = 1000, id = "") {
+  let a = document.createElement("a");
+  a.href = "#" + href;
+  a.setAttribute("onclick", "setFocusDelay('" + focus + "', " + focusDelay + ")");
+  a.id = id;
+  return a;
+}
+
+function H2() {
+  let h2 = document.createElement("h2");
+  return h2;
+}
+
+function Title(title = "TWICE", href = "Home", focus = "Home", focusDelay = 1000, id = "TwiceTitle") {
+  let h2 = H2(title);
+  let h2Link = Link(href, focus, focusDelay, id);
+  h2Link.innerHTML = title;
+  h2.appendChild(h2Link);
+  return h2;
+}
+
+function Picture(fileName = "Twice") {
+  let img = document.createElement("img");
+  img.setAttribute("data-src", "./img/webp/" + fileName + ".webp");
+
+  // Use .jpg or .png if browser doesn't support .webp images
+  img.onerror = () => {
+    img.onerror = () => {
+      img.onerror = () => {
+        console.log("Error: Could not find image of " + fileName + ".");
+      };
+      img.src = "./img/png/" + fileName + ".png";
+    };
+    img.src = "./img/jpg/" + fileName + ".jpg";
+  }
+
+  img.className = "lazyload";
+  return img;
+}
+
+function Div(className = "") {
+  let div = document.createElement("div");
+  div.className = className;
+  return div;
+}
+
+function PictureSide(memberName = "Nayeon") {
+  let pictureSide = Div("picture-side");
+  let img = Picture(memberName);
+  pictureSide.appendChild(img);
+  return pictureSide;
+}
+
+function Span(className = "", text = "") {
+  let span = document.createElement("span");
+  span.className = className;
+  span.innerHTML = text;
+  return span;
+}
+
+/**
+ * 
+ * @param {string} memberName 
+ * @param {object} infoList 
+ */
+function Info(memberName = "Nayeon", infoList = TwiceMemberInfo) {
+  let memberInfo = infoList[memberName]
+  let div = Div("info");
+  for (let info in memberInfo) {
+    let p = document.createElement("p");
+    let property = Span("property");
+
+    // Formatting profile information--thanks, Stack OverFlow
+    property.innerHTML = info.replace(/([A-Z]+)/g, " $1").replace(/([A-Z][a-z])/g, " $1").replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }) + ": ";
+
+    let value = Span("value", memberInfo[info]);
+    p.appendChild(property);
+    p.appendChild(value);
+    div.appendChild(p);
+  }
+  return div;
+}
+
+function InfoSide(memberName = "Nayeon", focusDelay = 1000) {
+  let infoSide = Div("info-side");
+  let title = Title(memberName, "Twice", memberName + "Link", focusDelay, memberName + "Title");
+  let info = Info(memberName);
+  infoSide.appendChild(title);
+  infoSide.appendChild(info);
+  return infoSide;
+}
+
+function TwicePictureList(members = TwiceMembers) {
+  let p = document.createElement("p");
+  p.className = "ignore";
+  for (let i = 0; i < members.length; i++) {
+    let memberLink = Link(members[i], members[i] + "Title", 800 + (i * 100), members[i] + "Link");
+    memberLink.innerHTML = members[i];
+    p.appendChild(memberLink);
+    if (i < members.length - 1) {
+      p.appendChild(document.createTextNode(", "));
+    }
+  }
+  return p;
+}
+
+function TwicePicture() {
+  let section, title, img, list;
+  section = Section(title = "Twice", tabIndex = 0);
+  title = Title();
+  img = Picture();
+  list = TwicePictureList();
+  section.appendChild(title);
+  section.appendChild(img);
+  section.appendChild(list);
+  return section;
+}
+
+function Profile(memberName = "Nayeon", swapSides = true, focusDelay = 1000) {
+  let section = Section(memberName);
+  section.id = memberName;
+  section.className += swapSides ? " left" : "";
+  let pictureSide = PictureSide(memberName);
+  let infoSide = InfoSide(memberName, focusDelay);
+  if (swapSides) {
+    section.appendChild(infoSide);
+    section.appendChild(pictureSide);
+  } else {
+    section.appendChild(pictureSide);
+    section.appendChild(infoSide);
+  }
+  return section;
+}
+
+/**
+ * Add this to the website.
+ * @param {string[]} members 
+ */
+function Profiles(members = TwiceMembers) {
+  let profiles = document.createElement("article");
+  profiles.appendChild(TwicePicture());
+  for (let i = 0; i < members.length; i++) {
+    let swapSides = i % 2 === 1; // Swap sides for every odd member
+    let profile = Profile(members[i], swapSides, 800 + (i * 100));
+    profiles.appendChild(profile);
+  }
+  return profiles;
 }
